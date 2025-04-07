410d9e5ba385f698b5865d3907ac4851545abf14
Fernanda Ishida <fer.ishida@gmail.com>
2025-04-06 21:00:33 -0300

    feat: implement task list with tRPC and types from utils

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -4,3 +4,8 @@ import { createTRPCReact } from '@trpc/react-query'
 import type { AppRouter } from '@/server/api/trpc'
 
 export const trpc = createTRPCReact<AppRouter>()
+
+//pega o retorno das rotas do backend e transforma em tipos que podem ser usados no front
+import type { inferRouterOutputs } from '@trpc/server'
+export type RouterOutputs = inferRouterOutputs<AppRouter>
+
