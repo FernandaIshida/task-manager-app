29fd97da70a3fb6eab1ad5aed1ce577ce0d1311c
Fernanda Ishida <fer.ishida@gmail.com>
2025-04-08 22:50:20 -0300

    chore: temporarily disable Tailwind usage due to issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-    content: ['./src/**/*.{ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['var(--font-inter)', 'sans-serif'],
+            },
             colors: {
                 background: '#f1f5f9',
                 surface: '#ffffff',
@@ -11,13 +18,11 @@ const config: Config = {
                 primaryHover: '#2563eb',
                 textPrimary: '#1e293b',
                 borderColor: '#cbd5e1',
-            },
-            fontFamily: {
-                sans: ['var(--font-inter)', 'sans-serif'],
+                debugPink: '#ff69b4',
+                green500: '#22c55e'
+
             },
         },
     },
     plugins: [],
-}
-
-export default config
+};
