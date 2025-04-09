106c0227e9a0f73c2a61a437955c27ce84d7c092
Fernanda Ishida <fer.ishida@gmail.com>
2025-04-08 22:47:55 -0300

    chore: update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,68 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextEslint from "@next/eslint-plugin-next";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    "plugin:@next/next/recommended": {}
+  }
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Configurações do Next.js
+  {
+    plugins: {
+      "@next/next": nextEslint,
+      "import": importPlugin
+    },
+    rules: {
+      ...nextEslint.configs.recommended.rules,
+      "@next/next/no-html-link-for-pages": "off" // Opcional: desativa regra específica
+    }
+  },
+
+  // Configurações globais
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        React: true,
+        JSX: true
+      }
+    }
+  },
+
+  // Regras personalizadas
+  {
+    files: [
+      "**/postcss.config.*",
+      "**/tailwind.config.*",
+      "**/eslint.config.*"
+    ],
+    languageOptions: {
+      parser: "@typescript-eslint/parser"
+    },
+    rules: {
+      "import/no-anonymous-default-export": [
+        "error",
+        {
+          allowObject: true,
+          allowArray: false,
+          allowArrowFunction: false,
+          allowAnonymousClass: false,
+          allowAnonymousFunction: false,
+          allowLiteral: false
+        }
+      ]
+    },
+  }
 ];
 
 export default eslintConfig;
